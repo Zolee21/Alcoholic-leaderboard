3ab871ac112c98e7c2fbc4504e22ef7e7fd59502
Zolee21 <zoledara@gmail.com>
2026-08-27 21:57:52 +0200

    V5.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'italpont-v5.5.0-shell-1';
+const CACHE_VERSION = 'italpont-v5.6.0-shell-1';
 const SHELL = [
   './','./index.html','./platform.js','./native.js','./pwa.js','./manifest.webmanifest',
   './assets/kulturfarm-banner.jpg','./assets/app-icon.png',
